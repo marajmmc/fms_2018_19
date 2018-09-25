27e3965befba59d3c1d5f1a4045bb9d5f88098cb
mahmud369 <mahmud369@gmail.com>
2018-09-25 13:17:52 +0600

    'LABEL_PURPOSE' added for Printing Setup

--- application/language/english/fms_lang.php
+++ application/language/english/fms_lang.php
@@ -185,3 +185,4 @@ $lang['LABEL_DEPARTMENT_NAME'] = 'Department';
 $lang['LABEL_NUMBER_OF_PAGE'] = 'Number of Page';
 $lang['LABEL_FILE_STATUS']="File Open/Close Status";
 $lang['LABEL_FILE_TOTAL_PERMISSION'] = "Total File Permission";
+$lang['LABEL_PURPOSE'] = 'Purpose';
