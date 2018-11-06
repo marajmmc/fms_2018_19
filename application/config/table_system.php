728db3194e6c27b63bcfa77b95184f795dc11057
Md. Shaiful Islam <islambuet@gmail.com>
2018-11-06 21:21:40 +0600

    Common session included

--- application/config/table_system.php
+++ application/config/table_system.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['table_system_assigned_group'] = 'arm_fms_2018_19.system_assigned_group';
 $config['table_system_history'] = 'arm_fms_2018_19.system_history';
 $config['table_system_history_hack'] = 'arm_fms_2018_19.system_history_hack';
-$config['table_system_session'] = 'arm_fms_2018_19.system_session';
+$config['table_system_session'] = 'arm_login_2018_19.system_session';
 $config['table_system_site_offline'] = 'arm_fms_2018_19.system_site_offline';
 $config['table_system_task'] = 'arm_fms_2018_19.system_task';
 $config['table_system_user_group'] = 'arm_fms_2018_19.system_user_group';
